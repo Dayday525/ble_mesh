da2b094c2368eec0af289c4b995a8b70afb907b9
mingliang1.yu <mingliang1.yu@tcl.com>
2021-01-11 16:56:09 +0800

    delete

--- nrf5_SDK_for_Mesh_v5.0.0_src/examples/light_switch/server/build/light_switch_server_nrf52832_xxAA_s132_7.2.0_Debug/obj/packed_index_list.d
+++
@@ -1,4 +0,0 @@
-build/light_switch_server_nrf52832_xxAA_s132_7.2.0_Debug/obj/packed_index_list.o: \
- C:\Users\mingliang1.yu\Desktop\ble_mesh\nrf5_SDK_for_Mesh_v5.0.0_src\models\foundation\config\src\packed_index_list.c \
- ../../../models/foundation/config/include/packed_index_list.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.10a/include/stdint.h
