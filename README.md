72b934bb6e3d30de79ce9dade01436ad70e83739
mingliang1.yu <mingliang1.yu@tcl.com>
2021-01-11 17:06:03 +0800

    initialize ble_mesh warehouse

---
+++ README.md
@@ -0,0 +1,2 @@
+# ble_mesh
+mingliang 20210111
---
+++ nrf5_SDK_for_Mesh_v5.0.0_src/examples/light_switch/server/build/light_switch_server_nrf52832_xxAA_s132_7.2.0_Debug/obj/enc.d
@@ -0,0 +1,59 @@
+build/light_switch_server_nrf52832_xxAA_s132_7.2.0_Debug/obj/enc.o: \
+ C:\Users\mingliang1.yu\Desktop\ble_mesh\nrf5_SDK_for_Mesh_v5.0.0_src\mesh\core\src\enc.c \
+ ../../../mesh/core/include/enc.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.10a/include/stdbool.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.10a/include/stdint.h \
+ ../../../mesh/core/include/ccm_soft.h ../../../mesh/core/api/nrf_mesh.h \
+ ../../../mesh/core/include/timer.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/ble.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/nrf_svc.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/nrf_error.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/ble_err.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/ble_gap.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/ble_hci.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/ble_ranges.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/ble_types.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/ble_l2cap.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/ble_gatt.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/ble_gattc.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf52.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/toolchain/cmsis/include/core_cm4.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/toolchain/cmsis/include/cmsis_version.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/toolchain/cmsis/include/cmsis_compiler.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/toolchain/cmsis/include/cmsis_gcc.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/toolchain/cmsis/include/mpu_armv7.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/system_nrf52.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf52_bitfields.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf51_to_nrf52.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/nrf52_name_change.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/compiler_abstraction.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/ble_gatts.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/ble_gap.h \
+ ../../../mesh/core/api/nrf_mesh_defines.h \
+ ../../../mesh/core/api/nrf_mesh_config_core.h \
+ include/nrf_mesh_config_app.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/nrf_sdm.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/nrf_error_sdm.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/nrf_soc.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/nrf_error_soc.h \
+ ../../../mesh/core/include/net_packet.h \
+ ../../../mesh/core/include/packet_mesh.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.10a/include/stddef.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.10a/include/__crossworks.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.10a/include/string.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/nrf_error.h \
+ ../../../mesh/core/include/rand.h ../../../mesh/core/include/aes.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/nrf_soc.h \
+ ../../../mesh/core/api/nrf_mesh_config_core.h \
+ ../../../mesh/core/include/aes_cmac.h \
+ ../../../mesh/core/include/ccm_soft.h ../../../mesh/core/include/utils.h \
+ ../../../mesh/core/include/hal.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/nrf52/nrf_mbr.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/softdevice/s132/headers/nrf_svc.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/libraries/util/app_util.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/modules/nrfx/mdk/compiler_abstraction.h \
+ C:/Users/mingliang1.yu/Desktop/ble_mesh/DeviceDownload/nRF5SDK1702d674dde/nRF5_SDK_17.0.2_d674dde/components/libraries/util/nordic_common.h \
+ ../../../mesh/core/api/nrf_mesh_assert.h \
+ ../../../mesh/core/api/nrf_mesh.h \
+ ../../../mesh/core/api/nrf_mesh_assert_gcc.h
